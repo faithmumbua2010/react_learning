01ff4c062e4b7f4ab508632885cca86acf4c5f7d
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-18 11:14:21 +0300

    feat: learning js classes

---
+++ class.js
@@ -0,0 +1,37 @@
+// class = provides more structured and cleaner way to 
+// work with objecrs compared to traditional constructor functions
+// eg. static keyword, encapsulation, inheritance
+
+/* class className(object){
+    constructorKeyword(parameters){... }
+
+    create a function---- inside a class youy do not need to use the function keyword
+}*/
+
+class product{
+    constructor(name, price){
+        this.name = name;
+        this.price= price;
+    }
+
+    displayProduct(){
+        console.log(`product: ${this.name}`);
+         console.log(`price: $${this.price.toFixed(2)}`);
+    }
+
+    calculateTotal(salesTax){
+      return this.price + (this.price * salesTax );
+    }
+}
+
+const salesTax = 0.05;
+
+const product1 = new product("shirt", 19.99);
+const product2 = new product("pants", 29.99);
+const product3 = new product("underwear", 100.00);
+
+product1.displayProduct();
+
+
+const total = product1.calculateTotal(salesTax);
+console.log(`Total price (with tax): $${total.toFixed(2)}`);
