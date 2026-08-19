aa3ef6ba418f208596d756d8ab711bbec9c22675
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-19 10:37:35 +0300

    feat:added the .map()function

---
+++ js_examples/map().js
@@ -0,0 +1,10 @@
+// .map()= accepts a callback and applies that function 
+// to each element of an array, then return a new array
+
+const numbers=[1, 2, 3, 4, 5];
+
+const squares = numbers.map(square);
+
+function square(element){
+    return Math.pow(element, 2)
+}
