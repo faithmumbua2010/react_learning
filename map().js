6f35ad338ffb774c500133dd5fed33de8586bf87
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-19 10:48:04 +0300

    feat:additional content to the maps

--- map().js
+++ map().js
@@ -4,7 +4,14 @@
 const numbers=[1, 2, 3, 4, 5];
 
 const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+
+console.log(squares);
 
 function square(element){
     return Math.pow(element, 2)
+}
+
+function cube(element){
+    return Math.pow(element, 3)
 }
