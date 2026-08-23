42c33a6a4859ad4f0f301a5a3c51b978c2187194
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-23 15:58:07 +0300

    feat:if statements

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,26 @@
+import { createRoot } from 'react-dom/client'
 
-import { createRoot } from "react-dom/client";
-import Car from "./Vehicle.jsx";
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
 
-createRoot(document.getElementById('root')).render(<Car />);
+function MadeGoal() {
+  return (
+    <>
+      <h1>Goal!</h1>
+      <p>Good job</p>
+    </>
+  );
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal/>;
+  }
+  return <MissedGoal/>;
+}
+
+createRoot(document.getElementById('root')).render(
+  <Goal isGoal={true} />
+);
