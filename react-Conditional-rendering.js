887c644d203d10e4fd5f8328fe69c675c9ebb309
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-23 15:51:10 +0300

    feat: if statements

--- react-Conditional-rendering.js
+++ react-Conditional-rendering.js
@@ -1,6 +1,14 @@
+
+
 //if Statement
 function Goal(props){
     if(isGoal){
         return <MadeGoal/>
     }
+    return <MissedGoal/>;
 }
+
+createRoot(document.getElementById('root')).render(
+    <Goal isGoal={false}/>
+);
+
