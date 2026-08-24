791a32eea1c5b5f4bd7359aaa58977263f28daa8
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-24 10:55:29 +0300

    feat:state hooks

--- hooks.js
+++ hooks.js
@@ -1,5 +1,7 @@
-/*Hooks- built-in JS functions that allow you to use state and other react features inside functional components 
+/*Hooks- built-in JS functions that  enable functional components to use state, lifecycle, and other React features without relying on class components.
 
 -ALL hook names are prefixed with the word 'use'
 
 */
+
+//State Hooks
