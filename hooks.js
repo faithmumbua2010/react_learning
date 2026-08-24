ba2dc96a3bf3e465cbf266592c6c761a3354788f
Faith Mumbua <faithmumbua2010@gmail.com>
2026-08-24 10:58:11 +0300

    feat: state hooks

--- hooks.js
+++ hooks.js
@@ -2,6 +2,11 @@
 
 -ALL hook names are prefixed with the word 'use'
 
+rules: they can only be called inside react function components
+     : can only be called at the top level of a component
+     : cannot be conditional
+
 */
 
 //State Hooks
+// . useState   . useReducer
